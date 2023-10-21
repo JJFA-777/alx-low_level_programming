774a0b0c0c23980365973ad0103aa8e51dcd5019
JJFA-777 <jesunboalade@gmail.com>
2023-10-21 03:34:49 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is to print a linr of string
+ *
+ * Retur: 0 upon success
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
