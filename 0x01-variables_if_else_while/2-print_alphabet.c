0c4240d064a73e73288005ddf2517a9d851af1ae
JJFA-777 <jesunboalade@gmail.com>
2023-10-22 02:47:04 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function to print the lowercase alphabet
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 'a' ; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
