dbdcab5d4c007ecc268cf9ab6b0d27e183c051a5
JJFA-777 <jesunboalade@gmail.com>
2024-06-29 12:13:05 -0700

    Task 4 betty compliance

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers 0-9 
- * excluding numbers 2 and 4
- *
- * @a - initialization of loop
- * 
- * Return: void
- */
+* print_most_numbers - print numbers 0-9
+* excluding numbers 2 and 4
+*
+* @a - initialization of loop
+*
+* Return: void
+*/
 
 void print_most_numbers(void)
 {
-        int a;
+	int a;
 
-        for (a = 0; a < 10; a++)
-        {
+	for (a = 0; a < 10; a++)
+	{
 		if (a == 2 || a == 4)
 		{
 			continue;
 		}
 		else
 		{
-                	_putchar('0' + a);
+			_putchar('0' + a);
 		}
-        }
-        _putchar('\n');
+	}
+	_putchar('\n');
 }
