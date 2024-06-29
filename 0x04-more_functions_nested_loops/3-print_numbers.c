b0ddb4c3436d5a2b048d59df020174f73a87a774
JJFA-777 <jesunboalade@gmail.com>
2024-06-29 11:45:02 -0700

    Whitespace betty corrections Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - print numbers 0-9
  * @a - initialization of loop
- * 
+ *
  * Return: void
  */
 
@@ -11,7 +11,7 @@ void print_numbers(void)
 {
 	int a;
 
-        for (a = 0; a < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		_putchar('0' + a);
 	}
