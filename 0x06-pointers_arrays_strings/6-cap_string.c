acc16c048bdc0ab350c79ed05ea228617cef4cc9
JJFA-777 <jesunboalade@gmail.com>
2024-07-08 15:28:22 -0700

    Task 6 corrections

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,13 +13,26 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	if (isalpha(str[i]))
+	if (str[i] >= 'a' && str[i] <= 'z')
 	{
 		str[i] = str[i] - 32;
 	}
 	while (str[i] != '\0')
 	{
-		if ((isalpha(str[i]) && islower(str[i])) && !(isalpha(str[i - 1])))
+		if ((str[i] >= 'a' && str[i] <= 'z') &&
+				(str[i - 1] == ' ' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}'))
 		{
 			str[i] = str[i] - 32;
 		}
