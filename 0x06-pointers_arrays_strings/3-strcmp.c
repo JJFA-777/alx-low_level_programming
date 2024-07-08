175d3667cef83b928ae5aab634841ca513a3304f
JJFA-777 <jesunboalade@gmail.com>
2024-07-07 19:35:42 -0700

    Task 4: correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,31 +6,20 @@
  * @s1: first comparative string.
  * @s2: Second comparative string.
  *
- * Return:	0 if string is the same.
- *		Difference between s1 and s2 if string is different.
+ * Return: Difference between s1 and s2 values.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, len1 = strlen(s1), len2 = strlen(s2);
+	int i = 0;
 
-	if (len1 != len2)
+	while ((s1[i] && s2[i]) != '\0')
 	{
-		return (s1[i] - s2[i]);
-	}
-	else
-	{
-		for (; i < len1 - 1; i++)
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] != s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-			else
-			{
-				continue;
-			}
+			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
