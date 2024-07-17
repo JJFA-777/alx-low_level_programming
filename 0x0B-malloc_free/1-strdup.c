cda08fdda591fffd71ab77456c1d3476079cf662
JJFA-777 <jesunboalade@gmail.com>
2024-07-16 18:33:04 -0700

    Task 1 correction: Initialized i

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - returns a pointer in memory which contains a copy of a the string.
- * @s: space in memeory.
  * @str: duplicate char.
  *
  * Return: pointer to duplicate string, or NULL if it fails.
@@ -12,7 +12,7 @@
 
 char *_strdup(char *str)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	char *s;
 
 	s = malloc(sizeof(str));
@@ -22,10 +22,12 @@ char *_strdup(char *str)
 		printf("Malloc failed");
 		return (NULL);
 	}
-	while (str[i] == '\0')
+	while (str[i] != '\0')
 	{
 		s[i] = str[i];
 		i++;
 	}
+	s[strlen(str)] = '\0';
+
 	return (s);
 }
