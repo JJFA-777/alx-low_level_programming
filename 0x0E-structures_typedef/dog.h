410e2ab8533ff0cc99dc49dff97953b9f0d097e5
JJFA-777 <jesunboalade@gmail.com>
2024-07-22 19:32:57 -0700

    Task 0: added comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure for a dog's info.
+ * @name: pointer to Dog's name.
+ * @age: floating point value for age.
+ * @owner: pointer to char string for dog's owner.
+ */
+
 struct dog {
 	char *name;
 	float age;
