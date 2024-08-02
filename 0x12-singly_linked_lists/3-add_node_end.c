c544a18a729f86e7a039dcae1fc32d3bd7193717
JJFA-777 <jesunboalade@gmail.com>
2024-08-02 04:33:02 +0000

    Task 3 betty and logic

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,46 +3,47 @@
 #include <string.h>
 
 /**
-* add_node_end - adds a new node at the end of a list_t list.
-* @str: Pointer to string to be duplicated.
-* @head: double pointer to head
-*
-* Return: the address of the new element, or NULL if it failed.
-*/
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @str: Pointer to string to be duplicated.
+ * @head: double pointer to head
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node, *trav;
-char *new_str;
-unsigned int len = 0;
+	list_t *new, *trav;
+	char *new_str;
+	unsigned int len = 0;
 
-new_str = strdup(str);
+	new_str = strdup(str);
+	if (new_str == NULL || head == NULL)
+		return NULL;
 
-if (new_str == NULL || head == NULL)
-return (NULL);
+	while (str[len])
+		len++;
 
-while (str[len])
-len++;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(new_str);
+		return NULL;
+	}
 
-new_node = malloc(sizeof(list_t));
-while (new_node == NULL)
-{
-free(new_str);
-return (NULL);
+	new->str = new_str;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		trav = *head;
+		while (trav->next != NULL)
+			trav = trav->next;
+		trav->next = new;
+	}
+
+	return (new);
 }
 
-if (*head == NULL)
-*head = new_node;
-else
-{
-trav = *head;
-while (trav->next != NULL)
-trav = trav->next;
-trav->next = new_node;
-new_node->str = new_str;
-new_node->len = len;
-new_node->next = NULL;
-}
-
-return (new_node);
-}
